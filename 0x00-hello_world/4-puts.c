9d6c59e98985d9a658ed611d996739907e0d1b7e
hk-jibrin <kasimharunajibrin@gmail.com>
2022-09-08 08:41:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+int main(void)
+{
+puts("\"programming is like a building a multilingual puzzle");
+return (0);
+}
