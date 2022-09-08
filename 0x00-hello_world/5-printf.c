629978119fdc91a33a2c6cc7ba54e2fe93c04dca
hk-jibrin <kasimharunajibrin@gmail.com>
2022-09-08 09:06:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    
- *    */
-int main(void)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int (main)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-5printfc
+a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
