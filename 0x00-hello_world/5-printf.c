442c59b30d5b63635189444bb19795bc520ce0d9
hk-jibrin <kasimharunajibrin@gmail.com>
2022-09-08 08:50:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  *
  *  * Description: main - Prints out a string to stdout.
@@ -8,13 +6,8 @@
  *   * Return: 0 if success.
  *
  *    */
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
